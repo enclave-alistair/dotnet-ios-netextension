1bc99efc7eade22c79b17fd949c6907db78dc70a
enclave-alistair <alistair.evans@enclave.io>
2023-09-18 12:02:58 +0100

    Move to net8.0-rc1

--- src/Dotnet.Test.Tray/install.sh
+++ src/Dotnet.Test.Tray/install.sh
@@ -4,4 +4,4 @@ deviceId=$(ios-deploy -c | grep -oE 'Found ([0-9A-Za-z\-]+)' | sed 's/Found //g'
 
 echo "Sending to device $deviceId"
 
-ios-deploy --id $deviceId --bundle bin/Release/net7.0-ios/ios-arm64/Dotnet.Test.Tray.app
+ios-deploy --id $deviceId --bundle bin/Release/net8.0-ios/ios-arm64/Dotnet.Test.Tray.app
--- src/Dotnet.Test.Tray/run.sh
+++ src/Dotnet.Test.Tray/run.sh
@@ -4,5 +4,5 @@ deviceId=$(ios-deploy -c | grep -oE 'Found ([0-9A-Za-z\-]+)' | sed 's/Found //g'
 
 echo "Sending to device $deviceId"
 
-dotnet build -t:Run -f net7.0-ios -c:Release -p:_DeviceName=$deviceId
+dotnet build -t:Run -f net8.0-ios -c:Release -p:_DeviceName=$deviceId
 
